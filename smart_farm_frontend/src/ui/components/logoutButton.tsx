8413bb5c0c23fc801793bc248a8a65b28a4caedb
Julian Schöpe <julian.schoepe@web.de>
2024-10-25 10:36:02 +0200

    added getUserprofileButton

--- smart_farm_frontend/src/ui/components/logoutButton.tsx
+++ smart_farm_frontend/src/ui/components/logoutButton.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import {Button} from "@mantine/core";
 import {useAuth} from "react-oidc-context";
+import axios from "axios";
 
 
 export const LogoutButton = () => {
     const auth = useAuth();
+
+
+    async function fetchData() {
+        try {
+            const token = auth.user?.access_token;
+            const response = await fetch(`http://127.0.0.1:8000/api/userprofiles`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.dir(response)
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     return (
         <>
         {
             auth.isAuthenticated &&
-            (<Button onClick={() => {
+            (<>
+                <Button onClick={() => {
                 void auth.signoutRedirect()
-            }} variant="filled" color="green">Logout</Button>)
+            }} variant="filled" color="green">Logout</Button>
+            <Button onClick={fetchData} variant="filled" color="green">getUserprofile</Button>
+                </>
+            )
         }
         </>
     )
