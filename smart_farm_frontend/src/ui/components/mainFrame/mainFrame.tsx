58ff439136ce1dc8935296f4bf346292ad9c16c8
Theo Lesser <t.lesser@tu-braunschweig.de>
2024-11-25 16:46:37 +0100

    removed div from mainFrame.

--- smart_farm_frontend/src/ui/components/mainFrame/mainFrame.tsx
+++ smart_farm_frontend/src/ui/components/mainFrame/mainFrame.tsx
@@ -1,48 +1,45 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import TimeseriesGraph from "../../../features/measurements/ui/timeseriesGraph";
 import placeholderImage from "../../../placeholder.png";
-import {Sensor} from "../../../features/sensor/models/Sensor";
-import {useLocation} from "react-router-dom";
-import {Fpf} from "../../../features/fpf/models/Fpf";
-import {receiveFpfData} from "../../../features/fpf/useCase/receiveFpfData";
-import {getFpf} from "../../../features/fpf/useCase/getFpf";
+import { Sensor } from "../../../features/sensor/models/Sensor";
+import { useLocation } from "react-router-dom";
+import { Fpf } from "../../../features/fpf/models/Fpf";
+import { getFpf } from "../../../features/fpf/useCase/getFpf";
+import { Container, Flex, Box, Image } from '@mantine/core';
 
 export const MainFrame = () => {
-
-    const [sensors, setSensors] = useState<Sensor[]>([])
-    const [fpf, setFpf] = useState<Fpf>()
+    const [sensors, setSensors] = useState<Sensor[]>([]);
+    const [fpf, setFpf] = useState<Fpf>();
 
     const { id } = useLocation().state;
 
     useEffect(() => {
-
-        console.log(id)
-        getFpf(id).then( resp => {
-            setFpf(resp)
-            console.log(resp)
-        })
+        console.log(id);
+        getFpf(id).then(resp => {
+            setFpf(resp);
+            console.log(resp);
+        });
     }, [id]);
 
-
     return (
-        <div style={{ display: 'flex', height: 'auto', width: '100vw' }}>
-            <div style={{ flexGrow: 1, padding: '20px', display: 'flex', flexDirection: 'column' }}>
-                <div style={{ display: 'flex', flexGrow: 1 }}>
-                    <div style={{ flex: 1, marginRight: '20px', overflowY: "scroll", maxHeight: "85vh", maxWidth: "50vw" }}>
+        <Container style={{ display: 'flex', height: 'auto', width: '100vw' }}>
+            <Flex style={{ flexGrow: 1, padding: '20px', flexDirection: 'column' }}>
+                <Flex style={{ flexGrow: 1 }}>
+                    <Box style={{ flex: 1, marginRight: '20px', overflowY: "scroll", maxHeight: "85vh", maxWidth: "50vw" }}>
                         {fpf && fpf.Sensors.map((sensor) => (
-                            <div key={sensor.id}>
-                                <TimeseriesGraph sensor={sensor}/>
-                            </div>
+                            <Box key={sensor.id}>
+                                <TimeseriesGraph sensor={sensor} />
+                            </Box>
                         ))}
-                    </div>
-                    <div style={{ width: '30%', display: 'flex', flexDirection: 'column' }}>
-                        <div style={{ height: 'auto', marginBottom: '20px'}}>
+                    </Box>
+                    <Box style={{ width: '30%', display: 'flex', flexDirection: 'column' }}>
+                        <Box style={{ height: 'auto', marginBottom: '20px' }}>
                             {/* Camera feed placeholder */}
-                            <img src={placeholderImage} alt="Placeholder" style={{width: '100%', height: 'auto'}}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                            <Image src={placeholderImage} alt="Placeholder" style={{ width: '100%', height: 'auto' }} />
+                        </Box>
+                    </Box>
+                </Flex>
+            </Flex>
+        </Container>
     );
 };
