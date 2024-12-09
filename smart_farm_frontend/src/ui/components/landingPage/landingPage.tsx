549cc362fd5db8e9f2aceeaa9d9ef03c835c45a6
Theo Lesser <t.lesser@tu-braunschweig.de>
2024-12-09 18:19:02 +0100

    Update

--- smart_farm_frontend/src/ui/components/landingPage/landingPage.tsx
+++ smart_farm_frontend/src/ui/components/landingPage/landingPage.tsx
@@ -1,15 +1,5 @@
-import {
-    Button,
-    Container,
-    Flex,
-    Group,
-    Menu,
-    rem,
-    TextInput,
-    Modal,
-    SimpleGrid, Card, Title, Image, Box, Pagination, Text
-} from '@mantine/core';
-import {IconChevronDown, IconZoomScan} from "@tabler/icons-react";
+import { Button, Container, Flex, Group, Menu, rem, TextInput, Modal, Card, Title, Image, Box, Pagination, Text, Grid } from '@mantine/core';
+import {IconChevronDown, IconZoomScan, IconDroplet, IconTemperature, IconSunHigh, IconCircleCheck, IconCircleMinus} from "@tabler/icons-react";
 import React, { PropsWithChildren, useEffect, useState } from "react";
 import { getMyOrganizations } from "../../../features/organization/useCase/getMyOrganizations";
 import { Organization } from "../../../features/organization/models/Organization";
@@ -17,18 +7,37 @@ import { useAuth } from "react-oidc-context";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../utils/store";
 import { OrganizationForm } from "../../../features/organization/ui/organizationForm";
-import { useNavigate } from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import { AppRoutes } from "../../../utils/appRoutes";
 import {Fpf} from "../../../features/fpf/models/Fpf";
 import placeholderImage from "../../../placeholder.png";
+import {getFpf} from "../../../features/fpf/useCase/getFpf";
+import {Sensor} from "../../../features/sensor/models/Sensor";
 
 const LandingPage: React.FC<PropsWithChildren<{}>> = ({ children }) => {
     const auth = useAuth();
+    const { organizationId, fpfId } = useParams();
     const [organizations, setOrganisations] = useState<Organization[]>([]);
     const [modalOpen, setModalOpen] = useState(false);
     const organizationEventListener = useSelector((state: RootState) => state.organization.createdOrganizationEvent);
     const navigate = useNavigate();
-    const [fpf, setFpf] = useState<Fpf>();
+    const [fpf, setFpf] = useState<Fpf>({id:"0", name:"", isPublic:true, Sensors:[], Cameras:[], sensorServiceIp:"", address:""});
+    const [sensors, setSensor]= useState<Sensor[]>()
+    const [fpfs, setFpfs] = useState([]);
+
+    useEffect(() => {
+        if(fpfId) {
+            getFpf(fpfId).then(resp => {
+                setFpf(resp)
+            })
+        }
+    }, [fpfId]);
+
+    useEffect(() => {
+        if(fpf?.Sensors && fpf.Sensors.length >= 1 ){
+            setSensor(fpf.Sensors)
+        }
+    }, [fpf]);
 
     useEffect(() => {
         if (auth.isAuthenticated) {
@@ -109,23 +118,148 @@ const LandingPage: React.FC<PropsWithChildren<{}>> = ({ children }) => {
                     </Group>
                 </Flex>
             </Container>
-            <Container>
-                <SimpleGrid cols={4} spacing="sm" verticalSpacing="sm">
-                    <Card p="lg" shadow="sm" radius="md" style={{ margin: '30px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.3)' }}>
-                        <Flex justify="space-between" align="center" mb="md">
+
+            {/*<Container style={{overflowY: "hidden"}}>
+                    <Grid>
+                        <Grid.Col span={4}>
+                        <Card p="lg" shadow="sm" radius="md" style={{ margin: '10px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.3)' }}>
+                        <Flex justify="space-between" align="center" mb="sm">
                             <Title order={3} style={{ color: '#199ff4' }}>{fpf?.name}</Title>
                             <Text c="blue">
-                                @ETCE
+                                {fpf.address}
                             </Text>
+                            <Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>
+                            <IconTemperature style={{ color: '#105385' }}/>
+                            <IconSunHigh style={{ color: '#105385' }}/>
+                            <IconDroplet style={{ color: '#105385' }}/>
+                            <Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>
+                            {fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>):("")}
+                            {fpf.sensor ? (<IconTemperature style={{ color: '#105385' }}/>):("")}
+                            {fpf.sensor ? (<IconSunHigh style={{ color: '#105385' }}/>):("")}
+                            {fpf.sensor ? (<IconDroplet style={{ color: '#105385' }}/>):("")}
+                            {fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>):("")}
                         </Flex>
 
-                        <Box style={{ height: 'auto', marginBottom: '20px' }}>
-                            {/* Camera feed placeholder */}
+                        <Box style={{ height: 'auto' }}>
                             <Image src={placeholderImage} alt="Placeholder" style={{ width: '100%', height: 'auto' }} />
                         </Box>
-                    </Card>
-                </SimpleGrid>
-                <Pagination total={10} siblings={1} defaultValue={1} />
+                    </Card></Grid.Col>*/}
+
+            <Container style={{overflowY: "hidden"}}>
+                <Grid>
+                    {fpfs.map((fpf) => (
+                        <Grid.Col span={4}>
+                            <Card p="lg" shadow="sm" radius="md" style={{ margin: '10px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.3)' }}>
+                                <Flex justify="space-between" align="center" mb="sm">
+                                    <Title order={3} style={{ color: '#199ff4' }}>{fpf.name}</Title>
+                                    <Text c="blue">{fpf.organization.address}</Text>
+                                    <Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>
+                                    <IconTemperature style={{ color: '#105385' }}/>
+                                    <IconSunHigh style={{ color: '#105385' }}/>
+                                    <IconDroplet style={{ color: '#105385' }}/>
+                                    <Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>
+                                    {/*{fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>):("")}
+                                    {fpf.sensor ? (<IconTemperature style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<IconSunHigh style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<IconDroplet style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>):("")}*/}
+                                </Flex>
+
+                                <Box style={{ height: 'auto' }}>
+                                    <Image src={fpf.lastImageUrl || placeholderImage} alt="Placeholder" style={{ width: '100%', height: 'auto' }} />
+                                </Box>
+                            </Card></Grid.Col>)}
+                        <Grid.Col span={4}>
+                            <Card p="lg" shadow="sm" radius="md" style={{ margin: '10px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.3)' }}>
+                                <Flex justify="space-between" align="center" mb="sm">
+                                    <Title order={3} style={{ color: '#199ff4' }}>Tolles FPF</Title>
+                                    <Text c="blue">
+                                        Goslar
+                                    </Text>
+                                    {/*{fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>):("")}
+                                    {fpf.sensor ? (<IconTemperature style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<IconSunHigh style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<IconDroplet style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>):("")}*/}
+                                </Flex>
+
+                                <Box style={{ height: 'auto' }}>
+                                    <Image src={placeholderImage} alt="Placeholder" style={{ width: '100%', height: 'auto' }} />
+                                </Box>
+                            </Card></Grid.Col>
+                        <Grid.Col span={4}>
+                            <Card p="lg" shadow="sm" radius="md" style={{ margin: '10px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.3)' }}>
+                                <Flex justify="space-between" align="center" mb="sm">
+                                    <Title order={3} style={{ color: '#199ff4' }}>{fpf?.name}</Title>
+                                    <Text c="blue">
+                                        {fpf.address}
+                                    </Text>
+                                    <Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>
+                                    <IconTemperature style={{ color: '#105385' }}/>
+                                    <IconSunHigh style={{ color: '#105385' }}/>
+                                    <IconDroplet style={{ color: '#105385' }}/>
+                                    <Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>
+                                    {/*{fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>):("")}
+                                    {fpf.sensor ? (<IconTemperature style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<IconSunHigh style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<IconDroplet style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>):("")}*/}
+                                </Flex>
+
+                                <Box style={{ height: 'auto' }}>
+                                    <Image src={placeholderImage} alt="Placeholder" style={{ width: '100%', height: 'auto' }} />
+                                </Box>
+                            </Card></Grid.Col>
+                        <Grid.Col span={4}>
+                            <Card p="lg" shadow="sm" radius="md" style={{ margin: '10px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.3)' }}>
+                                <Flex justify="space-between" align="center" mb="sm">
+                                    <Title order={3} style={{ color: '#199ff4' }}>{fpf?.name}</Title>
+                                    <Text c="blue">
+                                        {fpf.address}
+                                    </Text>
+                                    <Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>
+                                    <IconTemperature style={{ color: '#105385' }}/>
+                                    <IconSunHigh style={{ color: '#105385' }}/>
+                                    <IconDroplet style={{ color: '#105385' }}/>
+                                    <Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>
+                                    {/*{fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>):("")}
+                                    {fpf.sensor ? (<IconTemperature style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<IconSunHigh style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<IconDroplet style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>):("")}*/}
+                                </Flex>
+
+                                <Box style={{ height: 'auto' }}>
+                                    <Image src={placeholderImage} alt="Placeholder" style={{ width: '100%', height: 'auto' }} />
+                                </Box>
+                            </Card></Grid.Col>
+                        <Grid.Col span={4}>
+                            <Card p="lg" shadow="sm" radius="md" style={{ margin: '10px', boxShadow: '0 2px 8px rgba(0, 0, 0, 0.3)' }}>
+                                <Flex justify="space-between" align="center" mb="sm">
+                                    <Title order={3} style={{ color: '#199ff4' }}>{fpf?.name}</Title>
+                                    <Text c="blue">
+                                        {fpf.address}
+                                    </Text>
+                                    <Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>
+                                    <IconTemperature style={{ color: '#105385' }}/>
+                                    <IconSunHigh style={{ color: '#105385' }}/>
+                                    <IconDroplet style={{ color: '#105385' }}/>
+                                    <Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>
+                                    {/*{fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PPM</Text>):("")}
+                                    {fpf.sensor ? (<IconTemperature style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<IconSunHigh style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<IconDroplet style={{ color: '#105385' }}/>):("")}
+                                    {fpf.sensor ? (<Text size="xs" style={{ fontWeight: 'bold', color: '#105385' }}>PH</Text>):("")}*/}
+                                </Flex>
+
+                                <Box style={{ height: 'auto' }}>
+                                    <Image src={placeholderImage} alt="Placeholder" style={{ width: '100%', height: 'auto' }} />
+                                </Box>
+                            </Card></Grid.Col>
+                </Grid>
+                <Flex justify="center" mt="lg">
+                    <Pagination total={10} siblings={2} defaultValue={1} />
+                </Flex>
             </Container>
             <Modal
                 opened={modalOpen}
