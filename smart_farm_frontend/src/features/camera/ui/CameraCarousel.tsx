025d734e93e846ac1f54e0a558151d28b915d16d
Tom <t.heering@ostfalia.de>
2024-12-12 13:32:50 +0100

    Title Added

--- smart_farm_frontend/src/features/camera/ui/CameraCarousel.tsx
+++ smart_farm_frontend/src/features/camera/ui/CameraCarousel.tsx
@@ -3,12 +3,11 @@ import {Camera, EditCamera} from "../models/camera";
 import {useParams} from "react-router-dom";
 import { Carousel } from '@mantine/carousel';
 import {getImages} from "../useCase/getImages";
-import {getLivestream} from "../useCase/getLivestream";
-import { Image } from '@mantine/core';
-import ReactPlayer from 'react-player'
+import {Image, Title} from '@mantine/core';
 
 interface displayObject {
     url:string,
+    title:string,
     isLiveStream:boolean
 }
 
@@ -17,7 +16,7 @@ interface VideoPlayerProps {
 }
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ src }) => (
-    <video controls style={{ width: '100%', height: '18vw', maxWidth:'50vw' }} >
+    <video controls style={{ width: '100%', height: '18vw', maxWidth:'50vw'}} >
         <source src={src} type="video/mp4" />
         Your browser does not support the video tag.
     </video>
@@ -33,41 +32,56 @@ export const CameraCarousel:React.FC<{camerasToDisplay:Camera[]}> = ({camerasToD
             setObjectsToDisplay([])
             //For each Camera add all Images and Livestreams as objectsToDisplay
             camerasToDisplay.map((camera) => {
+                console.log(camera)
+                console.log(objectsToDisplay)
                 //If the camera has a SnapShot URL
-                if(camera.isActive && camera.snapshotUrl.length > 4){
+                if(camera.isActive){ // && camera.snapshotUrl.length > 1){
                     getImages(camera.id).then( resp => {
                         if (resp && resp.length > 0) {
+                            console.log(resp)
                             setObjectsToDisplay((prevObjects) => [
                                 ...prevObjects,
-                                { url: resp[0].url, isLiveStream: false },
+                                { url: resp[0].url, title: `${camera.name} Last Picture`, isLiveStream: false,   },
+                                { url: `${process.env.REACT_APP_BACKEND_URL}/api/cameras/${camera.id}/livestream`, title: `${camera.name} LiveStream`, isLiveStream: true }
                             ]);
                         }
                     })
                 }
                 //If the Camera has a Livestream URL
-                if(camera.isActive && camera.livestreamUrl.length > 4){
+                // Currently not needed as both URLS are required
+                /*
+                if(camera.isActive){
                     setObjectsToDisplay((prevObjects) => [
                         ...prevObjects,
                         { url: `${process.env.REACT_APP_BACKEND_URL}/api/cameras/${camera.id}/livestream`, isLiveStream: true },
                     ]);
                 }
+                */
+
             })
         }
     }, [fpfId, camerasToDisplay]);
 
+    let index = 0
+
     const slides = objectsToDisplay.map((objectToDisplay) => (
-        <Carousel.Slide>
+        <Carousel.Slide key={index}>
+            <Title order={3} style={{}} >
+                {objectToDisplay.title}
+            </Title>
             {!objectToDisplay.isLiveStream && (
-                <Image src={objectToDisplay.url} alt="Last Received Image" style={{ width: '100%', height: '18vw', maxWidth:'50vw' }} />
+                <Image src={objectToDisplay.url} alt="Last Received Image" fit="contain"  style={{ height: '18vw', maxWidth:'50vw' }} />
             )}
             {objectToDisplay.isLiveStream && (
                 <VideoPlayer src={objectToDisplay.url}/>
             )}
+            {index = index +1}
         </Carousel.Slide>
+
     ))
 
     return (
-        <Carousel>
+        <Carousel withIndicators>
             {slides}
             </Carousel>
     )
