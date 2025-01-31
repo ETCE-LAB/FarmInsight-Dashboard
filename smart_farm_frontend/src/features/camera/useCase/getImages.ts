6c8b5bc544d8164a1fc4eb93a917d1025ce4c9df
Tom <t.heering@ostfalia.de>
2025-01-31 12:24:53 +0100

    CameraCarrousel: Get Image only gathers images from last 30 days

--- smart_farm_frontend/src/features/camera/useCase/getImages.ts
+++ smart_farm_frontend/src/features/camera/useCase/getImages.ts
@@ -5,7 +5,21 @@ import {Measurement} from "../../measurements/models/measurement";
 
 
 
-export const getImages = (cameraId:string, from:string = "2024-10-10") => {
+export const getImages = (cameraId:string, from?:string) => {
+
+    if (!from) {
+        const currentDate = new Date();
+        const pastDate = new Date();
+        pastDate.setDate(currentDate.getDate() - 30);
+
+        // Formatierung des Datums im Format YYYY-MM-DD
+        const year = pastDate.getFullYear();
+        const month = String(pastDate.getMonth() + 1).padStart(2, '0');
+        const day = String(pastDate.getDate()).padStart(2, '0');
+
+        from = `${year}-${month}-${day}`;
+    }
+
     const apiClient = new APIClient()
 
     const user = getUser();
